28442900f3f56a2ba5192bbf8729d97aa3b64c15
gi-jeong1000 <pridess@dongguk.edu>
2023-03-30 13:44:29 +0900

    ossprac1

--- exercise.py
+++ exercise.py
@@ -1,5 +1,6 @@
 name = input("이름을 입력하세요: ")
 num = int(input("학번을 입력하세요: "))
 major = input("학과를 입력하세요: ")
+uni= input("학교를 입력하세요: ")
 print("<출력>")
-print(f"이름: {name}\n학번: {num}\n학과: {major}")
+print(f"이름: {name}\n학번: {num}\n학과: {major}\n학교: {uni}")
